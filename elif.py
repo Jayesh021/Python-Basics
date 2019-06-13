249138ead0e4a8ec74103617563e11fd22039ce6
Jayesh <jayeshsharma749@gmail.com>
2019-06-13 16:38:04 +0530

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,13 @@
+#elif
+
+x = int(input("enter the no. :"))
+y = int(input("enter the no. :"))
+z = int(input("enter the no. :"))
+
+
+if (x>y and x>z) :
+    print("x is greater")
+elif y>x and y>z :
+    print("y is greater")
+else :
+    print("z is greater")
